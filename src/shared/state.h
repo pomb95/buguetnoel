d9dfeaccc78663096b6f1219ef0dbd0035847e16
paulbuguet <paul.buguet@ensea.fr>
2017-10-03 09:47:51 +0200

    src

--- src/shared/state.h
+++ src/shared/state.h
@@ -2,5 +2,21 @@
 #ifndef __STATE__H
 #define __STATE__H
 
-#include "state/Exemple.h"
+#include "state/Element.h"
+#include "state/TypeId.h"
+#include "state/StaticELement.h"
+#include "state/MobileElement.h"
+#include "state/DIrection.h"
+#include "state/MInionStatus.h"
+#include "state/Minion.h"
+#include "state/HeroId.h"
+#include "state/HeroStatus.h"
+#include "state/Hero.h"
+#include "state/ObstacleId.h"
+#include "state/Obstacle.h"
+#include "state/Building.h"
+#include "state/CommandTower.h"
+#include "state/QG.h"
+#include "state/SpaceTypeId.h"
+#include "state/Space.h"
 #endif
---
+++ src/shared/state/Element.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include "Element.h"
+
+namespace state {
+
+    Element::Element() {
+        printf("Element ")
+    }
+    Element::~Element() {
+
+    }
+
+    const bool Element::equals(const Element& other) {
+
+    }
+    
+
+}
+
