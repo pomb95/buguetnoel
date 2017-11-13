777878fa86ac59bd215485e7a308b4f137413c2d
paulbuguet <paul.buguet@ensea.fr>
2017-11-13 15:06:27 +0100

    IA

---
+++ src/shared/ai/RandomAi.cpp
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <iostream>
+#include "RandomAi.h"
+
+ai::RandomAi::RandomAi() {}
+ai::RandomAi::~RandomAi() {}
+
+void ai::RandomAi::run(engine::Engine& engine, int character){
+    
+}
