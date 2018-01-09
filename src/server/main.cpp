40d88535b3825545521864f49aa5cdf00e562b0c
paulbuguet <paul.buguet@ensea.fr>
2018-01-09 11:49:58 +0100

    PORT 5050

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -230,15 +230,12 @@ int main (int argc, char* argv[]){
         servicesManager.registerService(make_unique<UserService>(std::ref(userDB)));
 
         struct MHD_Daemon *d;
-        if (argc != 2) {
-            printf("%s PORT\n", argv[0]);
-            return 1;
-        }
+       
         d = MHD_start_daemon(// MHD_USE_SELECT_INTERNALLY | MHD_USE_DEBUG | MHD_USE_POLL,
                 MHD_USE_SELECT_INTERNALLY | MHD_USE_DEBUG,
                 // MHD_USE_THREAD_PER_CONNECTION | MHD_USE_DEBUG | MHD_USE_POLL,
                 // MHD_USE_THREAD_PER_CONNECTION | MHD_USE_DEBUG,
-                atoi(argv[1]),
+                5050,
                 NULL, NULL, 
                 &main_handler, (void*) &servicesManager,
                 MHD_OPTION_NOTIFY_COMPLETED, request_completed, NULL,
